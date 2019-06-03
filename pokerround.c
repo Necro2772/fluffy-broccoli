fe0d82249f78b1bca9b848dd7065c16085f7ffc7
Michael Roberts <robertsm8923@my.sduhsd.net>
2019-06-03 11:04:59 -0700

    Started file system

---
+++ pokerround.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void main(int a, char *input[]){
+  FILE *fp;
+  int deck[52];
+  int value;
+  int i = 0;
+
+  fp = fopen ("./cards.txt", "rb");
+  if (fp) {
+    while(1){
+      value = fgetc (fp);
+      if (isdigit(value)) {
+        printf("%d", value);
+        deck[i] = value;
+      }
+      i++;
+      if (value == ']') break;
+    }
+    while(1){
+      value = fgetc (fp);
+      if (value == ']') break;
+    }
+    while(1){
+      value = fgetc (fp);
+      if (value == ']') break;
+    }
+    fclose (fp);
+
+  }
+
+
+}
