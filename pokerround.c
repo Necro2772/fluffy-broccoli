c9bd26e0ac2cbe0e27660b6a8b0ceb148be900f2
Michael Roberts <robertsm8923@my.sduhsd.net>
2019-06-04 10:11:16 -0700

    updated

--- pokerround.c
+++ pokerround.c
@@ -4,6 +4,7 @@ void main(int a, char *input[]){
   FILE *fp;
   int deck[52];
   int value;
+  int num = 0;
   int i = 0;
 
   fp = fopen ("./cards.txt", "rb");
@@ -12,9 +13,13 @@ void main(int a, char *input[]){
       value = fgetc (fp);
       if (isdigit(value)) {
         printf("%d", value);
-        deck[i] = value;
+        num = num * 10;
+        num += value;
       }
+      if (value == ',') {
+      deck[i] = num;
       i++;
+      }
       if (value == ']') break;
     }
     while(1){
