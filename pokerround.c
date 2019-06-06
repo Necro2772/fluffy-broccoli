fa0e4d79069fbdc91e1c740f8dd31f14c0a4bf6e
Michael Roberts <robertsm8923@my.sduhsd.net>
2019-06-06 10:00:57 -0700

    save me

--- pokerround.c
+++ pokerround.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <time.h>
 
-
-void shuffle(int *cards[]){
+void shuffle(int* cards){
+  srand(time(0));
+  int repeat = 0;
   for (int i = 0; i < 52; i++){
-    *cards[i] = rand() % 52;
-    printf("%d\n", *cards[i]);
+    cards[i] = rand() % 52;
+    while(1){
+      repeat = 0;
+      for(int a = 0; a < i; a++){
+        if(cards[i] == cards[a]){
+          repeat = 1;
+        }
+      }
+      if(repeat == 1){
+        cards[i] = rand() % 52;
+      }else{
+        break;
+      }
+    }
   }
 
+  for(int i = 0; i < 52; i++){
+  printf("%d\n", cards[i]);
+  }
 }
 
-void main(int a, char *input[]){
+void main(int b, char *input[]){
   FILE *fp;
   int deck[52];
   int player[52];
   int dealer[52];
   int value;
   int num = 0;
-  int i = 0;
+  int a = 0;
 
   fp = fopen ("./cards.txt", "rb");
   if(fp){
@@ -30,12 +47,13 @@ void main(int a, char *input[]){
         num += (int)value;
       }
       if (value == ',') {
-      deck[i] = num;
-      i++;
+      deck[a] = num;
+      a++;
       num = 0;
       }
       if (value == ']') break;
     }
+    a = 0;
     while(1){
       value = fgetc (fp);
       if (isdigit(value)) {
@@ -44,12 +62,13 @@ void main(int a, char *input[]){
         num += (int)value;
       }
       if (value == ',') {
-      player[i] = num;
-      i++;
+      player[a] = num;
+      a++;
       num = 0;
       }
       if (value == ']') break;
     }
+    a = 0;
     while(1){
       value = fgetc (fp);
       if (isdigit(value)) {
@@ -58,8 +77,8 @@ void main(int a, char *input[]){
         num += (int)value;
       }
       if (value == ',') {
-      dealer[i] = num;
-      i++;
+      dealer[a] = num;
+      a++;
       num = 0;
       }
       if (value == ']') break;
@@ -71,7 +90,8 @@ void main(int a, char *input[]){
 
 
 
-  if (&deck[1] == NULL){
+  if (!(isdigit(player[1]))){
     shuffle(deck);
   }
+  printf("%d\n", sizeof(deck));
 }
