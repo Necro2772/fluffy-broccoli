a1389e3b732ea13cb7210de796684dc373ce985f
Necro2772 <50212290+Necro2772@users.noreply.github.com>
2019-06-09 13:51:28 -0700

    Created

---
+++ getcard.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char main(int a, char* input[]){
+  int num;
+  num = input[2];
+  
+  switch(num){
+    case 0:
+    case 1:
+    case 2:
+    
+    
+    
+    
+    }
+  
+  
+  
+}
