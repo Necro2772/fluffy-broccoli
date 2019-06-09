6e3d6d8bd8f4098000f4c214827dad45b42a759d
Dposs777 <50212294+Dposs777@users.noreply.github.com>
2019-06-09 14:07:09 -0700

    hand hurts

--- getcard.c
+++ getcard.c
@@ -5,15 +5,57 @@ char main(int a, char* input[]){
   num = input[2];
   
   switch(num){
-    case 0:
-    case 1:
-    case 2:
-    
-    
-    
-    
-    }
-  
-  
+    case 0: return "https://drive.google.com/open?id=11F9Q-7SRm10FWZ2s53USrmToqPGm-Uls";
+    case 1: return "https://drive.google.com/open?id=118dIphiaQ4vjMDud--frPHrd3bSgeOlY";
+    case 2: return "https://drive.google.com/open?id=11DcUAxlv2XMjmW3KWRFiC87B9Dsm90-i";
+    case 3: return "https://drive.google.com/open?id=10OLVwjUskkjdxTFsAv6BeRFklgai6Wjj";
+    case 4: return "https://drive.google.com/open?id=1-bVhbwHsl5kO-gYVmbB41uFLJyFvr3VI";
+    case 5: return "https://drive.google.com/open?id=1-7vHxGkWxtEXsqMIY_U28vDxpVuKJ9pN";
+    case 6: return "https://drive.google.com/open?id=1-d-uVNDlzmXUY0j1gBlXg1kEDslsF1fS";
+    case 7: return "https://drive.google.com/open?id=1-OfTCyrTwv78azYCFE6Y5bY9XEVGQaTV";
+    case 8: return "https://drive.google.com/open?id=1-RadMd6DdPLz53XE8HzTsqrAD19hfAkB";
+    case 9: return "https://drive.google.com/open?id=1-3USsP9-8IoEbesHjN-I1gxV4HxnD4yw";  
+    case 10: return "https://drive.google.com/open?id=1-T5vgkHELzMNKPIjG7sqLP-qlD-J4zDW";  
+    case 11: return "https://drive.google.com/open?id=1-boHuZVstH1uD-kob1JsU1yGg4C9niw9";  
+    case 12: return "https://drive.google.com/open?id=1-ezVxsdypdi9aJlqD6EbVt0UaqMOfh5X";  
+    case 13: return "https://drive.google.com/open?id=1-jzX20BfUeSLfYBFllMls-OSjhMC4yhA";  
+    case 14: return "https://drive.google.com/open?id=1-rJCh-M_8a7eRcHl-I-b6iqIwhn3ixQi";  
+    case 15: return "https://drive.google.com/open?id=1-xClWqvP7IdVacGGVy2jhEVrstT9rfhv";  
+    case 16: return "https://drive.google.com/open?id=104tDEcxsfTs-zQG86nbQFJdII0GpsciE";  
+    case 17: return "https://drive.google.com/open?id=10-xJPn2-MRyCANC_IGOFPQbqPsL18feU";  
+    case 18: return "https://drive.google.com/open?id=10Anllr3rXwOsuZ5RpuVU5tOuP7kolFCQ";  
+    case 19: return "https://drive.google.com/open?id=11wnBOtkY24i3jaA9bG9HyXoyHHBrV2t2";  
+    case 20: return "https://drive.google.com/open?id=10MI_XKgShOBtRkq0V8SDPs2DwnI8P2SZ";  
+    case 21: return "https://drive.google.com/open?id=10lAem6hZ36LwB1cNtqmdJLuo-drWcMM4";  
+    case 22: return "https://drive.google.com/open?id=10PLKo51HwWVemJDSTt2l_-PBW2kschIN";
+    case 23: return "https://drive.google.com/open?id=10_Pls348Ln2LjlfRIrTmOpos8WBoTPKk";
+    case 24: return "https://drive.google.com/open?id=10eeGRvjxJFGTvhRsknpBAbA9wdx1xEnW";
+    case 25: return "https://drive.google.com/open?id=11wdh-lrxgYA8q2whvmB4HxECg-R6Gjnk";
+    case 26: return "https://drive.google.com/open?id=10Dlp8zpoLN5kglxq-1-PAzhYUDRH08oY";  
+    case 27: return "https://drive.google.com/open?id=10Hv-Ofu5eLd7bcSZy3RBQCenM58fDRG0";  
+    case 28: return "https://drive.google.com/open?id=10awWctNHhCHetVV5N4N55JTvUhyxdCnn";  
+    case 29: return "https://drive.google.com/open?id=104zwzXtEn_u6ebvwVUWIj2bs9k76TzG3";  
+    case 30: return "https://drive.google.com/open?id=111mMe7HuY3lR3pze9bANSMuKNuqtK7Pv";  
+    case 31: return "https://drive.google.com/open?id=10eYxMlTn0hiw-JRuwOyd288wEio1Kvms";
+    case 32: return "https://drive.google.com/open?id=111Q49k12obxEi7A_wDAgITeOb7dBO28O";
+    case 33: return "https://drive.google.com/open?id=10g56g8lXWyvz26xlSr-mTrHf0DMjz1_S";
+    case 34: return "https://drive.google.com/open?id=10lDtKIa49CqrPCJBEBfAlv7kLrWvBkRe";
+    case 35: return "https://drive.google.com/open?id=11-jjxn8Lm09Qp3pqFK-CtBUsUtKMDbDj";
+    case 36: return "https://drive.google.com/open?id=1-0JwG_tQ4D1PpSYPR9xr96SaZ3Mx8o8l";
+    case 37: return "https://drive.google.com/open?id=1-13Z7NIZMpEFhF1SYh2p_xdV767MCXCi";
+    case 38: return "https://drive.google.com/open?id=1-4Zhva6zCvTesvzLPDXcUtZzyKGNJDxo";
+    case 39: return "https://drive.google.com/open?id=1-SniCBewyNmi1Eaz0qR3z8qU45jXKQYE";
+    case 40: return "https://drive.google.com/open?id=11FMOt51DY9lrND13QipoO6cHae6jK0TK";
+    case 41: return "https://drive.google.com/open?id=11W7Y9T8Evhg95aIeWQcID-HoP3HpzB-1";
+    case 42: return "https://drive.google.com/open?id=11_y7EG5lfHLCgNladupIgP6CKTGFWgzY";
+    case 43: return "https://drive.google.com/open?id=11dTvjqwbLneQrLRw2J_8IhK30QyCzPtl";
+    case 44: return "https://drive.google.com/open?id=12KiPzagF589Mfdfv-eeth57uw1TEtoij";
+    case 45: return "https://drive.google.com/open?id=12NlHPwWVIU2lNjpjjkmeySaSCWVhttSA";
+    case 46: return "https://drive.google.com/open?id=12YxjWW5NAeZgkcBCRtEHl5A_C_480lb9";
+    case 47: return "https://drive.google.com/open?id=12yXIhdx_pmehqbdYdcX-hGuTz9WAFZCr";
+    case 48: return "https://drive.google.com/open?id=12JuGyZhTsCEqUX9d8HVviDkdfjQU4gtd";
+    case 49: return "https://drive.google.com/open?id=11pJ0xJXxgURoFkeFiXG_9ZOLQABieGhK";
+    case 50: return "https://drive.google.com/open?id=120v954ps_H-DaezyaRaUU-s2rTkpVlfG";
+    case 51: return "https://drive.google.com/open?id=12F3uNqNX7OlbnERiGk7abeX0FF9zi15v";
   
 }
