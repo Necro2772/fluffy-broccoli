d04a42007a7a22835ebb6a72b0eafe1932fb0740
Necro2772 <50212290+Necro2772@users.noreply.github.com>
2019-06-09 11:20:00 -0700

    Fixed reading files

--- pokerround.c
+++ pokerround.c
@@ -14,7 +14,7 @@ int main(int b, char *input[]){
   int a = 0;
   int top;
 
-  fp = fopen ("./cards.txt", "rb");
+  fp = fopen ("./cards", "rb");
   if(fp){
     while(1){
       value = fgetc (fp);
@@ -24,27 +24,25 @@ int main(int b, char *input[]){
         num += (int)value;
       }
       if (value == ',') {
-      deck[a] = num;
-      a++;
-      num = 0;
+        deck[a] = num;
+        a++;
+        num = 0;
       }
       if (value == ']') break;
     }
     a = 0;
     while(1){
       value = fgetc (fp);
-      if (isdigit(value)) {
+      if (value == '-'){
+        num = -1;
+      }
+      if (isdigit(value) && num != -1) {
         value -= 48;
         num = num * 10;
         num += (int)value;
       }
       if (value == ',') {
         player[a] = num;
-        a++;
-        num = 0;
-      }
-      if (value == '-'){
-        player[a] = -1;
         a++;
         num = 0;
       }
@@ -58,18 +56,16 @@ int main(int b, char *input[]){
     a = 0;
     while(1){
       value = fgetc (fp);
-      if (isdigit(value)) {
+      if (value == '-'){
+        num = -1;
+      }
+      if (isdigit(value) && num != -1) {
         value -= 48;
         num = num * 10;
         num += (int)value;
       }
       if (value == ',') {
-      dealer[a] = num;
-      a++;
-      num = 0;
-      }
-      if (value == '-'){
-        dealer[a] = -1;
+        dealer[a] = num;
         a++;
         num = 0;
       }
@@ -80,16 +76,24 @@ int main(int b, char *input[]){
       }
       if (value == ']') break;
     }
+    while(1){
+      value = fgetc (fp);
+      if (isdigit(value)) {
+        value -= 48;
+        num = num * 10;
+        num += (int)value;
+      }
+      if (value == ']') {
+        top = num;
+        break;
+      }
+    }
     fclose (fp);
   }
-
-  if (!(isdigit(player[1]))){
-    shuffle(deck);
-    top = 51;
-  }
-
-  switch (*input[1]){
+  switch (*input[2]){
     case 'r':
+      shuffle(deck, player, dealer);
+      top = 51;
       draw(&top, deck, player);
       draw(&top, deck, player);
       draw(&top, deck, dealer);
@@ -97,30 +101,35 @@ int main(int b, char *input[]){
       break;
     case 'd':
       draw(&top, deck, player);
+      printf("Player: %d\n", score(player));
       if (score(player) > 21){
         printf("Your hand is over 21, you lose!\n");
+        return 3;
       }
       break;
     case 's':
       while (score(dealer) < 16){
         draw(&top, deck, dealer);
       }
+      printf("Player: %d, Dealer: %d\n", score(player), score(dealer));
       if (score(dealer) > 21){
         printf("Dealer busts, player wins!\n");
+        return 2;
       }
       else if (score(player) >= score(dealer)){
         printf("Player wins!\n");
+        return 2
       }
       else{
         printf("Dealer wins!\n");
+        return 3
       }
       break;
     default:
       break;
   }
 
-  fp = fopen ("./cards.txt", "wb");
-  
+  fp = fopen ("cards", "wb");
   if(fp){
     fprintf(fp, "deck[");
     for(int i = 0; i < 52; i++){
@@ -128,15 +137,17 @@ int main(int b, char *input[]){
     }
     fprintf(fp, "]\n");
     fprintf(fp, "player[");
-    for(int i = 0; i < (51 - top); i++){
+    for(int i = 0; i < 52; i++){
       fprintf(fp, "%d,", player[i]);
     }
     fprintf(fp, "]\n");
     fprintf(fp, "dealer[");
-    for(int i = 0; i < (51 - top); i++){
+    for(int i = 0; i < 52; i++){
       fprintf(fp, "%d,", dealer[i]);
     }
     fprintf(fp, "]\n");
+    fprintf(fp, "top[");
+    fprintf(fp, "%d]", top);
   }
   fclose(fp);
 }
