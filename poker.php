669542dc4887c328e873f8c39fa7608af3c2a3f4
Daniel Possemato <possematod8884@my.sduhsd.net>
2019-06-10 10:32:14 -0700

    safkjsaufgas

--- poker.php
+++ poker.php
@@ -9,71 +9,13 @@
 
     <h1>Poker</h1>
     <p>Input Reset, Draw, or Stay</p>
-    
-    <?php
-        $a = $num = 0;
-        $fp = fopen ("./cards.txt", "r");
-          while(1){
-            $value = fgetc ($fp);
-            if($value == ']') break;
-          }
-          while(1){
-            $value = fgetc ($fp);
-            if ($value == '-'){
-              $num = -1;
-            }
-            if (isdigit($value) && $num != -1) {
-              $value -= 48;
-              $num = $num * 10;
-              $num += (int)$value;
-            }
-            if ($value == ',') {
-              //$player[$a] = $num;
-              exec("./getcard " . $num . $output, $retc);
-              echo "<img src=\"" . $output . "\" alt=\"card\">";
-              
-              
-              $a++;
-              $num = 0;
-            }
-            if ($value == ']' && $a == 0){
-              for ($i = 0; $i<52; $i++){
-                $player[$i] = -1;
-              }
-            }
-            if ($value == ']') break;
-          }
-          $a = 0;
-          while(1){
-            $value = fgetc ($fp);
-            if ($value == '-'){
-              $num = -1;
-            }
-            if (isdigit($value) && $num != -1) {
-              $value -= 48;
-              $num = $num * 10;
-              $num += (int)$value;
-            }
-            if ($value == ',') {
-              //$dealer[$a] = $num;
-              exec("./getcard " . $num . $output, $retc);
-              echo "<img src=\"" . $output . "\" alt=\"card\">";
-              
-              
-              $a++;
-              $num = 0;
-            }
-            if ($value == ']') break;
-          }
-          fclose ($fp);
-    ?>
-    
+
     <?php
        // define variables and set to empty values
        $arg1 = $output = $retc = "";
        if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $arg1 = test_input($_POST["arg1"]);
-         exec("./pokerround " . $arg1 . $output, $retc); 
+         exec("./pokerround " . $arg1 , $output, $retc);
        }
        function test_input($data) {
          $data = trim($data);
@@ -92,15 +34,9 @@
     <?php
        // only display if return code is numeric - i.e. exec has been called
        if (is_numeric($retc)) {
-         if($output == 2){
-           echo "<h2>Player wins!</h2>";
+         foreach ($output as $line) {
+           echo "<h2>$line</h2>";
          }
-         if($output == 3){
-           echo "<h2>Dealer wins!</h2>";
-         }
-       
-         echo "<h2>Program Return Code:</h2>";
-         echo $retc;
        }
     ?>
     
